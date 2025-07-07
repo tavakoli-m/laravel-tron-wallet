5cf76e1a7b31c8ecfc385e3d0ebe12f0afff7217
unknown <heyreza8@gmail.com>
2025-07-08 01:33:52 +0330

    handling api http exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Api\ApiResponse\Facades\ApiResponse;
 use App\Http\Middleware\RequireJsonHeader;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+// Exceptions
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Illuminate\Auth\Access\AuthorizationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +27,45 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        // 422 status code
+        $exceptions->render(function (ValidationException $exception, Request $request) {
+            return ApiResponse::withStatus(422)
+                ->withErrors($exception->errors())
+                ->withMessage($exception->getMessage())
+                ->send();
+        });
+
+        // 401 status code (Sanctum)
+        $exceptions->render(function (AuthenticationException $exception, Request $request) {
+            return ApiResponse::withStatus(401)
+                ->withMessage($exception->getMessage())
+                ->send();
+        });
+
+        // 403 status code
+        $exceptions->render(function (AccessDeniedHttpException $exception, Request $request) {
+            if ($exception->getPrevious() instanceof AuthorizationException) {
+                return ApiResponse::withStatus(403)
+                    ->withMessage("Unauthorized.")
+                    ->send();
+            }
+        });
+
+        // 404 status code
+        $exceptions->render(function (NotFoundHttpException $exception, Request $request) {
+            return ApiResponse::withStatus(404)
+                ->withMessage("Not Found !!")
+                ->send();
+        });
+
+        // 405 status code
+        $exceptions->render(function (MethodNotAllowedHttpException $exception, Request $request) {
+            return ApiResponse::withStatus(405)
+                ->withMessage("Method Not Allowed !!")
+                ->send();
+        });
+
+        $exceptions->shouldRenderJsonWhen(function (Request $request) {
+            return true;
+        });
     })->create();
