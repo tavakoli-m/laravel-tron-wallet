55ec2e90a642c7b9ef9ce55c83751bb24d855822
unknown <heyreza8@gmail.com>
2025-07-08 01:37:59 +0330

    init v1 api endpoints

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 // Exceptions
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
@@ -16,10 +17,14 @@ use Illuminate\Auth\Access\AuthorizationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
+        using: function () {
+            Route::middleware('api')
+                ->domain('api.' . env('APP_URL'))
+                ->group(function () {
+                    Route::prefix('v1')->group(base_path('routes/api_v1.php'));
+        });
+    },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
